e0b6cb46bbf689b3f7c4fcbd2a8de61fe053a6c0
alan m <alan@ubernetics.com>
2014-10-26 20:28:16 -0600

    Change to use variables for each player's name

--- studio_game.rb
+++ studio_game.rb
@@ -2,7 +2,9 @@
 #
 name1 = 'larry'
 health1 = 60
-puts "Players:\n\tlarry\n\tcurly\n\tmoe"
+name2 = 'curly'
+name3 = 'moe'
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
 #puts "#{name1}'s health is #{health1 / 9.0}"
 
 
