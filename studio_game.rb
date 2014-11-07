5d634677518801bc823bd0cd710f0183a6ba4c2a
alan m <alan@ubernetics.com>
2014-11-07 14:26:26 -0600

    Capitalize player#1 name: larry

--- studio_game.rb
+++ studio_game.rb
@@ -4,7 +4,7 @@ name1 = 'larry'
 health1 = 60
 name2 = 'curly'
 name3 = 'moe'
-puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
-#puts "#{name1}'s health is #{health1 / 9.0}"
+#puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+puts "#{name1.capitalize} has a health of #{health1}."
 
 
