7e6137168c2036bfe1bdb3a270f399daa019e422
alan m <alan@ubernetics.com>
2014-11-07 14:50:55 -0600

    Added Player#3 Moe and printed in screen centered with *'s

--- studio_game.rb
+++ studio_game.rb
@@ -5,6 +5,7 @@ health1 = 60
 name2 = 'curly'
 health2 = 125
 name3 = 'moe'
+health3= 100
 
 #puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
 puts "#{name1.capitalize} has a health of #{health1}."
@@ -17,3 +18,5 @@ health1 = 30
 puts "#{name1.capitalize} has a health of #{health1}."
 puts "#{name2.upcase} has a health of #{health2}."
 
+title = "#{name3.capitalize} has a health of #{health3}."
+puts title.center(50, "*")
