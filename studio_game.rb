f5ca0ccc405b164788449d4e7a40089a95abb707
alan m <alan@ubernetics.com>
2014-11-07 14:33:05 -0600

    player#2 upcase and Curly heaklth make the same as larry's

--- studio_game.rb
+++ studio_game.rb
@@ -3,8 +3,14 @@
 name1 = 'larry'
 health1 = 60
 name2 = 'curly'
+health2 = 125
 name3 = 'moe'
+
 #puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
 puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
 
+health2 = health1
+
+puts "#{name2.upcase} has a health of #{health2}."
 
