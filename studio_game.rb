22b3dae408abf95def7613a9a49c6872a958d811
alan m <alan@ubernetics.com>
2014-11-07 14:40:44 -0600

    reasign variable to another

--- studio_game.rb
+++ studio_game.rb
@@ -11,6 +11,9 @@ puts "#{name1.capitalize} has a health of #{health1}."
 puts "#{name2.upcase} has a health of #{health2}."
 
 health2 = health1
+puts "#{name2.upcase} has a health of #{health2}."
 
+health1 = 30
+puts "#{name1.capitalize} has a health of #{health1}."
 puts "#{name2.upcase} has a health of #{health2}."
 
