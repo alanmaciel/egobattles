09cacbc6dabf94c55eced4c291659424989be58a
alan m <alan@ubernetics.com>
2014-11-07 15:03:48 -0600

    Added Player#4 and printed in screen with left justification of 30 dots

--- studio_game.rb
+++ studio_game.rb
@@ -5,7 +5,9 @@ health1 = 60
 name2 = 'curly'
 health2 = 125
 name3 = 'moe'
-health3= 100
+health3 = 100
+name4 = 'Shemp'
+health4 = 90
 
 #puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
 puts "#{name1.capitalize} has a health of #{health1}."
@@ -20,3 +22,6 @@ puts "#{name2.upcase} has a health of #{health2}."
 
 title = "#{name3.capitalize} has a health of #{health3}."
 puts title.center(50, "*")
+
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+
