277ef4e97fef243e993a5dc2613fbb37e7835c62
alan m <alan@ubernetics.com>
2014-11-24 14:26:32 -0600

    Starting with a clean slate

--- studio_game.rb
+++ studio_game.rb
@@ -1,13 +1,3 @@
-def now
-  time = Time.new
-  time.strftime('%I:%M:%S')
+class Player
+  
 end
-
-def say_hello(name, health=100)
-  "I'm #{name.capitalize} with a health of #{health} as of #{now}."
-end
-
-puts say_hello("larry", 60)
-puts say_hello("curly", 125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
