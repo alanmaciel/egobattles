6027f8ea1c20f9abbf49e48f1216c2be4ee263a2
alan m <alan@ubernetics.com>
2014-11-25 12:37:32 -0600

    Added say_hello method instance method to Player's object

--- studio_game.rb
+++ studio_game.rb
@@ -2,7 +2,12 @@ class Player
   def initialize(name, health=100)
     @name, @health = name.capitalize, health
   end
+
+  def say_hello
+    "I'm #{@name} with a health of #{@health}."
+  end
 end
 
 player1 = Player.new("moe")
 puts player1.inspect
+puts player1.say_hello
