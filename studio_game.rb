f2218c3b59d1bea9b94e065b1207f6e7235c0eeb
alan m <alan@ubernetics.com>
2014-11-25 12:25:01 -0600

    Added capitalize method before assigning name variable so initialize method put the object in a 'good usable state', Added the inspect method to the puts.

--- studio_game.rb
+++ studio_game.rb
@@ -1,8 +1,8 @@
 class Player
   def initialize(name, health)
-    @name, @health = name, health
+    @name, @health = name.capitalize, health
   end
 end
 
 player1 = Player.new("moe", 100)
-puts player1
+puts player1.inspect
