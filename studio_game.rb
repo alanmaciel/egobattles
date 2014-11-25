2bdc85a8a6dd787c8573f78b3ef4da118d7491db
alan m <alan@ubernetics.com>
2014-11-25 15:03:45 -0600

    Rename say_hello instance method to to_s

--- studio_game.rb
+++ studio_game.rb
@@ -3,7 +3,7 @@ class Player
     @name, @health = name.capitalize, health
   end
 
-  def say_hello
+  def to_s
     "I'm #{@name} with a health of #{@health}."
   end
 
@@ -22,10 +22,10 @@ player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-puts player1.say_hello
-puts player2.say_hello
-puts player3.say_hello
+puts player1
+puts player2
+puts player3
 player3.blam
-puts player3.say_hello
+puts player3
 player3.w00t
-puts player3.say_hello
+puts player3
