24a0d0fd198a8121253082bc4b051bfca92cddd8
alan m <alan@ubernetics.com>
2014-12-08 18:29:53 -0600

    Added price_in_cents virtual attribute

--- classes.rb
+++ classes.rb
@@ -11,11 +11,13 @@ class BookInStock
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
   end
+
+  def price_in_cents
+    Integer(price*100 + 0.5)
+  end
+
 end
 
 book = BookInStock.new("isbn1", 33.80)
-puts "ISBN  = #{book.isbn}"
-puts "Price = #{book.price}"
-book.price = book.price * 0.75
-puts "New price = #{book.price}"
-
+puts "Price          = #{book.price}"
+puts "Price in cents = #{book.price_in_cents}"
