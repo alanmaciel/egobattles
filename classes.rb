bd6ed5b527d51a5851d6d1999b417928e3e12cba
alan m <alan@ubernetics.com>
2014-12-08 18:07:21 -0600

    changed  isbn and price defs for attr_reader

--- classes.rb
+++ classes.rb
@@ -1,20 +1,16 @@
 class BookInStock
+
+  attr_reader :isbn, :price
+
   def initialize(isbn, price)
     @isbn   = isbn
     @price  = Float(price)
   end
-  
+
   def to_s
     "ISBN: #{@isbn}, price: #{@price}"
   end
 
-  def isbn
-    @isbn
-  end
-
-  def price
-    @price
-  end
 end
 
 book = BookInStock.new("isbn1", 12.34)
