85e1f08745d17de61aeb764376faf5285c61ce27
alan m <alan@ubernetics.com>
2014-12-10 12:17:07 -0600

    Classes working with other classes

---
+++ classes/csv_reader.rb
@@ -0,0 +1,25 @@
+class CsvReader
+  def initialize
+    @books_in_stock = []
+  end
+
+  def read_in_csv_data(csv_file_name)
+    CSV.foreach(csvfile_name, headers: true) do |row|
+      @books_in_stock << BookInStock.new(row["ISBN"], row["Price"])
+    end
+  end
+
+  def total_value_in_stock
+    
+  end
+
+  def number_of_each_isbn
+    
+  end
+end
+
+reader = CsvReader.new
+reader.read_in_csv_data("file1.csv")
+reader.read_in_csv_data("file2.csv")
+
+puts "Total value in stock = #{reader.total_value_in_stock}"
