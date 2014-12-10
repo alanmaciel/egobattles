85904f69cac05cc779beb7e982f79fba2a436d24
alan m <alan@ubernetics.com>
2014-12-10 15:07:11 -0600

    Attributes, add writable (setters) and readable (getters) methods and a virtual attribute

--- studio_game.rb
+++ studio_game.rb
@@ -1,10 +1,13 @@
 class Player
+  attr_reader :health
+  attr_accessor :name
+
   def initialize(name, health=100)
     @name, @health = name.capitalize, health
   end
 
   def to_s
-    "I'm #{@name} with a health of #{@health}."
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
   end
 
   def blam
@@ -16,16 +19,35 @@ class Player
     @health += 15
     puts "#{@name} got w00ted!"
   end
+
+  def score
+    @health + @name.length
+  end
+
+  def name=(new_name)
+    @name = new_name.capitalize
+  end
 end
 
 player1 = Player.new("moe")
-player2 = Player.new("larry", 60)
-player3 = Player.new("curly", 125)
-
 puts player1
+puts player1.health
+
+player2 = Player.new("larry", 60)
+puts player2.name
+player2.name = "lawrence"
+puts player2.name
+puts player2.health
+puts player2.score
 puts player2
+
+player3 = Player.new("curly", 125)
 puts player3
+puts player3.health
+
 player3.blam
 puts player3
 player3.w00t
 puts player3
+
+
