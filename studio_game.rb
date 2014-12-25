768c87fe7927d72863d738524b60368b09f5e2c5
alan m <alan@ubernetics.com>
2014-12-24 18:30:07 -0600

    Redesign to OO, added a Game class to interact with other players (objects)

--- studio_game.rb
+++ studio_game.rb
@@ -29,43 +29,41 @@ class Player
   end
 end
 
+class Game
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+    puts "There are #{@players.size} players in #{@title}:"
+    @players.each do |player|
+      puts player
+    end
+
+    @players.each do |player|
+      player.blam
+      player.w00t
+      player.w00t
+      puts player
+    end
+  end
+end
+
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 player4 = Player.new("shemp", 90)
-players = [player1, player2, player3]
 
-players.pop
-players.push(player4)
-puts "There are #{players.size} players in the game:"
-
-players.each do |player|
-  player.blam
-  player.w00t
-  player.w00t
-  puts player
-end
-
-#players.each do |player|
-#  puts player.health
-#end
-
-#puts player1
-#puts player1.health
-#
-#puts player2.name
-#player2.name = "lawrence"
-#puts player2.name
-#puts player2.health
-#puts player2.score
-#puts player2
-#
-#puts player3
-#puts player3.health
-#
-#player3.blam
-#puts player3
-#player3.w00t
-#puts player3
-
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
 
